93c980d096f29ff37e4cf190e56510c6c92fd455
German Lopez <gerleven@gmail.com>
2022-07-15 12:26:05 -0300

    diferentes tipos de props

--- hola-mundo/src/App.js
+++ hola-mundo/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Componente from './Components/Componente';
+import Propiedades from './Components/Propiedades';
 
 function App() {
   return (
@@ -8,20 +9,22 @@ function App() {
       <header className="App-header">
         <section>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </section>
         <section>
           <Componente mensaje="Este Mensaje viene desde el padre"></Componente>
+        </section>
+        <section>
+          <Propiedades
+            cadena = "Una cadena"
+            numero = {99}
+            booleano = {true}
+            arreglo = {[1,2,3]}
+            objeto = {{curso: "React", donde: "Diveria"}}
+            funcion = {(num)=>{return num*num}}
+            elementoReact = {<h6>Elemento React</h6>}
+            componenteReact = {<Componente mensaje="Componente pasado como Prop"/>}
+          />
+          {console.table(Propiedades)}
         </section>
       </header>
     </div>
