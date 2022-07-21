4502cc4dbe20b5025a6e7ba16cd692af20753cac
German Lopez <gerleven@gmail.com>
2022-07-21 15:26:03 -0300

    componente contador con hook useState

--- hola-mundo/src/Components/ContadorHooks.jsx
+++ hola-mundo/src/Components/ContadorHooks.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ContadorHooks = (props) => {
+  // Declaración de una variable de estado que llamaremos "cuenta"
   const [cuenta, setCuenta] = useState(0);
+
   //const ["estado", "funcion para actualizar el estado con prefijo SET"] = useState("valor inicial del estado");
 
   //desaparecen todos los this.
   //Ahora en vez de usar this.state directamente usamos la variable cuenta
 
-  //Duda: y si en mi estado quiero tener mas de una variable? seria un objeto y dentro del objeto todas las properties?
+  //Duda: y si en mi estado quiero tener mas de una variable?
+  //En ese caso llamamos varias veces a useState
+  //por ejemplo, si quiero otra variale de estado llamada fruit:
+  const [fruit, setFruit] = useState("banana");
+  const [persona, setPersona] = useState({ name: "German", age: 33 });
+
+  // De forma similar a componentDidMount y componentDidUpdate
+  useEffect(() => {
+    // Actualiza el título del documento usando la API del navegador
+    document.title = `You clicked ${cuenta} times`;
+    //document.title es el titulo que aparece en el nombre de la pestaña en el navegador
+  });
 
   const sumar = () => {
     setCuenta(cuenta + 1);
