fb031516880738260f22f4319a49210799aef15c
German Lopez <gerleven@gmail.com>
2022-08-02 10:40:26 -0300

    Como usar BrowserRouter, Routes y Route

--- react-router-v6/src/App.js
+++ react-router-v6/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "./Pages/AboutPage";
+import HomePage from "./Pages/HomePage";
+import UsersPage from "./Pages/UsersPage";
 
 const App = () => {
-  return <>Hello World!</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/users" element={<UsersPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ react-router-v6/src/Pages/UsersPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UsersPage = () => {
+  return (
+    <>
+      <h3>Users!</h3>
+    </>
+  );
+};
+
+export default UsersPage;
