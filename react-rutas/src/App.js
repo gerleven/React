fff3761ce119717796c101a0fcf50de736d859b3
German Lopez <gerleven@gmail.com>
2022-08-02 01:06:48 -0300

    Videos 5 y 6 terminados

--- react-rutas/src/App.js
+++ react-rutas/src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { RuteoBasico } from "./components/RuteoBasico";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Manejo de Rutas</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-header">
+          <RuteoBasico></RuteoBasico>
+        </div>
       </header>
     </div>
   );
