0b53e1951a47b9cc4c83cb2d3e08e6aae40b072b
German Lopez <gerleven@gmail.com>
2022-08-02 16:37:02 -0300

    Navigate para redirigir a otra Route, atributo replace para no apilar

--- react-router-v6/src/App.js
+++ react-router-v6/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, NavLink, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink, Link, Navigate } from "react-router-dom";
 import AboutPage from "./Pages/AboutPage";
 import HomePage from "./Pages/HomePage";
 import UsersPage from "./Pages/UsersPage";
@@ -18,12 +18,20 @@ const App = () => {
   return (
     <BrowserRouter>
       {/* <Navbar /> */}
-      <NavLink to="/user/10">go to /user/10</NavLink>
+      {/* <NavLink to="/user/10">go to /user/10</NavLink><br></br>
+      <NavLink to="/user/10/Juan">go to /user/10/Juan</NavLink><br></br> */}
+      {/* <NavLink to="/users">go to /users</NavLink><br></br>
+      <NavLink to="/about">go to /about</NavLink><br></br> */}
+      <NavLink to="/usuarios">go to /usuarios</NavLink><br></br>
+      <NavLink to="/usuariosSinRetorno">go to /usuarios y sin retorno</NavLink><br></br>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="/usuarios" element={<Navigate replace to="/users" />} />
+        <Route path="/usuariosSinRetorno" element={<Navigate to="/users" />} />
+        {/* El replace hace que /usuarios sea reemplazado por /users en vez de ser apilado encima, al volver con el navegador volverias al "/"*/}
         <Route path="/user/:id" element={<UserPage />} />
         <Route path="/user/:id/:name" element={<UserPage />} />
         <Route path="/others" element={<p>Others!</p>} />
--- react-router-v6/src/Pages/UserPage.js
+++ react-router-v6/src/Pages/UserPage.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 
 const UserPage = () => {
-  const params = useParams();
   const { id, name } = useParams();
   
   return (
     <>
-        <h3>User {params.id} {name!=undefined?<span>- {params.name}</span>:<></>}</h3>
         <h3>User {id} {name!=undefined?<span>- {name}</span>:<></>}</h3>
         <Link to="/">Back</Link>
     </>
