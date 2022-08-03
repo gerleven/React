98c5e58ab81b8873f0b09f3fd4183c36a0dfcf4a
German Lopez <gerleven@gmail.com>
2022-08-03 10:13:48 -0300

    subComponents o componentes anidados

--- react-router-v6/src/Pages/Dashboard.js
+++ react-router-v6/src/Pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link, Routes, Route } from "react-router-dom";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -12,6 +12,22 @@ const Dashboard = () => {
     <>
       <h3>Dashboard</h3>
       <button onClick={handleClick}>Go Home</button>
+      <br></br>
+      <button onClick={()=>{navigate("welcome")}}>Go to subComponent Welcome</button>
+      <br></br>
+      <button onClick={()=>{navigate("options")}}>Go to subComponent Options</button>
+      <br></br>
+
+      <Link to="welcome">Go to subComponent Welcome</Link>
+      <br></br>
+      <Link to="options">Go to subComponent Options</Link>
+      <br></br>
+      <br></br>
+
+      <Routes>
+        <Route path="/welcome" element={<h3>Welcome!</h3>} /> {/*En el proximo ejercicio vemos como hacer para no tener los Route distribuidos en los subcomponentes y tenerlos todos juntos en el App.js*/}
+        <Route path="/options" element={<h3>Options!</h3>} />
+      </Routes>
     </>
   );
 };
