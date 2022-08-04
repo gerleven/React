b903baf0224ac0e4aafa491686b43e91c341cc19
German Lopez <gerleven@gmail.com>
2022-08-04 14:20:05 -0300

    add styles and Routes

--- mis-canciones/src/App.js
+++ mis-canciones/src/App.js
@@ -4,7 +4,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import {
   BrowserRouter,
   Routes,
-  Route,
   NavLink,
   Link,
   Navigate,
@@ -12,8 +11,12 @@ import {
   useParams,
   useNavigate,
   useLocation,
+  Route,
 } from "react-router-dom";
 import Header from "./Components/Header";
+import Eror404 from "./Pages/Error404";
+import Home from "./Pages/Home";
+import Cancion from "./Pages/Cancion";
 
 function App() {
   return (
@@ -21,11 +24,12 @@ function App() {
       <CssBaseline>
         <div className="App">
           <Header />
+
           <main className="App-main">
             <Routes>
-              {/* <Route></Route>
-              <Route></Route>
-              <Route></Route> */}
+              <Route path="/" element={<Home/>}/>
+              <Route path="/cancion/:id" element={<Cancion/>}/>
+              <Route path="*" element={<Eror404/>}/>
             </Routes>
           </main>
         </div>
---
+++ mis-canciones/src/Pages/Error404.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+const Eror404 = () => {
+  const url = useLocation();
+  const navigate = useNavigate();
+  return (
+    <>
+      <Stack sx={{ width: "100%" }} spacing={2}>
+        <Alert
+          severity="error"
+          onClose={() => {
+            navigate("/");
+          }}
+          action={
+            <Button color="inherit" size="small" onClick={()=>{navigate("/")}}>
+              Volver
+            </Button>
+          }
+        >
+          <AlertTitle>Pagina no encontrada</AlertTitle>
+          La url <b>'{url.pathname}'</b> no fue encontrada
+        </Alert>
+      </Stack>
+    </>
+  );
+};
+
+export default Eror404;
