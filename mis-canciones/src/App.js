d8da0285a4a5dc5439cdedf135086160f4632d32
German Lopez <gerleven@gmail.com>
2022-08-07 22:53:18 -0300

    variables de estado iniciales

--- mis-canciones/src/App.js
+++ mis-canciones/src/App.js
@@ -17,8 +17,23 @@ import Header from "./Components/Header";
 import Eror404 from "./Pages/Error404";
 import Home from "./Pages/Home";
 import Cancion from "./Pages/Cancion";
+import { React, useEffect, useState } from "react";
 
 function App() {
+  //Variables
+  let mySongsInit = JSON.parse(localStorage.getItem("mySongs"));
+  let searchInit = {
+    artist: "",
+    songs: "",
+    request: false
+  };
+
+  //Variables de estado
+  const [mySongs, setMySongs] = useState(mySongsInit); //snippet: usf
+  const [search, setSearch] = useState(searchInit);
+  const [currentSong, setCurrentSong] = useState({});
+  const [error, setError] = useState(false);
+
   return (
     <BrowserRouter>
       <CssBaseline>
@@ -27,9 +42,9 @@ function App() {
 
           <main className="App-main">
             <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/cancion/:id" element={<Cancion/>}/>
-              <Route path="*" element={<Eror404/>}/>
+              <Route path="/" element={<Home />} />
+              <Route path="/cancion/:id" element={<Cancion />} />
+              <Route path="*" element={<Eror404 />} />
             </Routes>
           </main>
         </div>
