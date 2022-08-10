ee2c8b1141fd4ab7bd8c03a239981743c3bd4646
German Lopez <gerleven@gmail.com>
2022-08-10 09:50:31 -0300

    add icons in artist and song fileds

--- mis-canciones/src/Components/Buscador.js
+++ mis-canciones/src/Components/Buscador.js
@@ -4,6 +4,9 @@ import { Button } from "@mui/material";
 import { IconButton } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
+import { AccountCircle } from '@mui/icons-material';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const Buscador = () => {
   return (
@@ -17,13 +20,21 @@ const Buscador = () => {
       <IconButton color="primary" size="small" type="reset">
         <HomeIcon />
       </IconButton>
-      
+
       <TextField
+        
         id="artist"
         name="artist"
         label="Artista"
         variant="standard"
         required
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
       />
       <TextField
         id="song"
@@ -31,6 +42,13 @@ const Buscador = () => {
         label="Cancion"
         variant="standard"
         required
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <MusicNoteIcon />
+            </InputAdornment>
+          ),
+        }}
       />
       <IconButton color="primary" size="small" type="submit">
         <SearchIcon/>
