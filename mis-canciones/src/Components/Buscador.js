671bbda2a61589782ffa5e3e05cee2bfcf157d1c
German Lopez <gerleven@gmail.com>
2022-08-10 10:16:44 -0300

    aesthetic fixes

--- mis-canciones/src/Components/Buscador.js
+++ mis-canciones/src/Components/Buscador.js
@@ -4,9 +4,9 @@ import { Button } from "@mui/material";
 import { IconButton } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
-import { AccountCircle } from '@mui/icons-material';
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
-import InputAdornment from '@mui/material/InputAdornment';
+import { AccountCircle } from "@mui/icons-material";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import InputAdornment from "@mui/material/InputAdornment";
 
 const Buscador = () => {
   return (
@@ -14,15 +14,21 @@ const Buscador = () => {
       component="form"
       sx={{
         "& > :not(style)": { m: 1, width: "25ch" },
+        "& > *": {
+          margin: 0,
+          width: "auto",
+        },
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
       autoComplete="on"
     >
-      <IconButton color="primary" size="small" type="reset">
+      <IconButton color="primary" size="small" type="reset" style={{width: "30px", marginTop: "30px"}}>
         <HomeIcon />
       </IconButton>
 
       <TextField
-        
         id="artist"
         name="artist"
         label="Artista"
@@ -50,8 +56,8 @@ const Buscador = () => {
           ),
         }}
       />
-      <IconButton color="primary" size="small" type="submit">
-        <SearchIcon/>
+      <IconButton color="primary" size="small" type="submit" style={{width: "30px", marginTop: "30px"}}>
+        <SearchIcon />
       </IconButton>
     </Box>
   );
