f6c079de90f717853c13d8a1c3b3d60b8194c2a2
german.levental <german.levental@diveria.com>
2022-09-07 12:13:17 -0300

    update

--- mis-canciones/src/App.js
+++ mis-canciones/src/App.js
@@ -78,7 +78,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <HashRouter basename="/React/">
+      <HashRouter basename="/React">
         <CssBaseline>
           <div className="App-backgroundColor">
             <div className="App">
